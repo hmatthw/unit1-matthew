e725019ad01288d84ddcef1682b9d54542f8fd19
hmatthw <matthewh403@nycstudents.net>
2026-09-14 10:21:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,39 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+
+t.shape('turtle')
+t.forward(200)
+
+def message(input):
+    print(input)
+message("Hello Class")
+
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(200)
+
+def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+equal(200) 
+
+def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
+right()
+
+turtle.done()
